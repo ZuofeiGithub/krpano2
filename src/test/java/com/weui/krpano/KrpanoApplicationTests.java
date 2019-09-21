e8e836eeab293ea4afb47ef648c5c6f981334740
zuofei <348068347@qq.com>
2019-09-21 17:56:10 +0800

    xml生成

--- src/test/java/com/weui/krpano/KrpanoApplicationTests.java
+++ src/test/java/com/weui/krpano/KrpanoApplicationTests.java
@@ -4,6 +4,16 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +21,52 @@ public class KrpanoApplicationTests {
 
     @Test
     public void contextLoads() {
+        Long start = System.currentTimeMillis();
+        createXml();
+        System.out.println("运行时间：" + (System.currentTimeMillis() - start));
     }
 
+    public static void createXml() {
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = factory.newDocumentBuilder();
+            Document document = documentBuilder.newDocument();
+            document.setXmlStandalone(true);
+            Element bookstore = document.createElement("bookstore");
+            // 向bookstore根节点中添加子节点book
+            Element book = document.createElement("book");
+
+            Element name = document.createElement("name");
+            // 不显示内容 name.setNodeValue("不好使");
+            name.setTextContent("雷神");
+            book.appendChild(name);
+            // 为book节点添加属性
+            book.setAttribute("id", "1");
+            // 将book节点添加到bookstore根节点中
+            bookstore.appendChild(book);
+            // 将bookstore节点（已包含book）添加到dom树中
+            document.appendChild(bookstore);
+
+            // 创建TransformerFactory对象
+            TransformerFactory tff = TransformerFactory.newInstance();
+            // 创建 Transformer对象
+            Transformer tf = tff.newTransformer();
+
+            // 输出内容是否使用换行
+            tf.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            System.out.println(document.toString());
+            // 创建xml文件并写入内容
+            tf.transform(new DOMSource(document), new StreamResult(new File("book1.xml")));
+            System.out.println("生成book1.xml成功");
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
 }
